efb217e5986323ebba586dc16891560f0d6e739c
HGattermeyer <henrique1989@gmail.com>
2021-04-11 17:18:15 -0300

    adding pergormance optimizations to codebase with react lazy, suspense

--- client/src/components/collections-overview/collections-overview.component.jsx
+++ client/src/components/collections-overview/collections-overview.component.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import "./collections-overview.styles.scss";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+import { CollectionsOverviewContainer } from "./collections-overview.styles";
 
 const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
-    {collections.map(({ id, ...othersCollectionsProps }) => (
-      <CollectionPreview key={id} {...othersCollectionsProps} />
+  <CollectionsOverviewContainer>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-  </div>
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
